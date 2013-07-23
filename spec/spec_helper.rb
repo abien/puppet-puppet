e4ffa81c7a3795b5981422d936b51e94da2f834d
Alexander Bien <abien@gmx.net>
2013-07-23 13:34:17 +0200

    cleanup leftovers

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,19 @@
-require 'puppetlabs_spec_helper/module_spec_helper'
+dir = File.expand_path(File.dirname(__FILE__))
+$LOAD_PATH.unshift File.join(dir, 'lib')
 
-fixture_path = File.expand_path(File.join(__FILE__, '..', 'fixtures'))
+require 'mocha'
+require 'puppet'
+require 'rspec'
+require 'spec/autorun'
 
-RSpec.configure do |c|
-  c.module_path = File.join(fixture_path, 'modules')
-  c.manifest_dir = File.join(fixture_path, 'manifests')
-  c.mock_with :mocha
+Spec::Runner.configure do |config|
+    config.mock_with :mocha
 end
+
+# We need this because the RAL uses 'should' as a method.  This
+# allows us the same behaviour but with a different method name.
+class Object
+    alias :must :should
+end
+
+
